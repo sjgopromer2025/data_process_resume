334382b138f62dfc41aece604f8da84592f47b8a
유승진 <91680691+sjgopromer2025@users.noreply.github.com>
2025-01-25 11:07:29 +0900

    for resume

--- src/main.py
+++ src/main.py
@@ -1,8 +1,1 @@
-from analysis.gps.csv_common_gps_road2 import csv_gps_road_time
-
-csv_gps_road_time("DalgoT", "sinnonhyun-gangnam-road", ["20240901"], 16, 1, 0, 30)
-
-
-# from map.map_rectangle import total_road_map
-
-# total_road_map()
+# 읽기 전용
